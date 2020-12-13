fe5ef8b8d92c16766fe9e4508aad8ca3a9d88a8f
ruifernandees <dev.ruifernandes@gmail.com>
2020-12-13 09:28:27 -0300

    Implement Csv class, rawCsv and toJson methods

---
+++ src/Csv.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace RuiF\CsvToJson;
+
+class Csv
+{
+    private $rawCsv;
+
+    public function __construct(array $rawCsv)
+    {
+        $this->rawCsv = $rawCsv;    
+    }
+
+    public function rawCsv()
+    {
+        return $this->rawCsv;
+    }
+
+    public function toJson(string $dataName)
+    {
+        $newArray = [];
+        foreach ($this->rawCsv as $item) {
+            $stringWithoutLineBreak = str_replace("\n", "", $item);
+            $newArray[] = explode(",", $stringWithoutLineBreak);
+        }
+
+        $finalArray = [];
+
+        for ($iterator = 1; $iterator < count($newArray); $iterator++) {
+            $finalArray[$iterator - 1] = [];
+            for ($secIterator = 0; $secIterator < count($newArray[0]); $secIterator++) {
+                $finalArray[$iterator - 1][$newArray[0][$secIterator]] = $newArray[$iterator][$secIterator];
+            }
+        }
+
+        return json_encode([ $dataName => $finalArray ]);
+    }
+}
